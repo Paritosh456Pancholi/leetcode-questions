1758e116ab685c8b01923dfac88fd713a8b62a3a
Paritosh Pancholi <55954820+Paritosh456Pancholi@users.noreply.github.com>
2022-07-16 23:05:24 +0530

    Time: 7 ms (19.66%), Space: 8.7 MB (61.83%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -5,10 +5,13 @@ public:
         for(int i=n-1;i>=0;i--)
         {
             if(digits[i]<9)
-            {digits[i]++ ; return digits;} 
+            {digits[i]++ ; 
+             return digits;} 
             else
                 digits[i]=0;
         }
+        //initiliziing vector ofsize n+ with all value 0
+        //if the num is 999 then this case will run
         vector<int>newnumber(n+1,0);
         newnumber[0]=1;
         return newnumber;
